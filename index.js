86138a8c20adf6a191ff77fe544ef8b4d76d7f9b
Serkan Aysan <serkanaysann@gmail.com>
2017-08-11 15:55:02 +0300

    io url changed

--- index.js
+++ index.js
@@ -11,15 +11,6 @@ var server = app.listen(8080, function(){
 //Static files
 app.use(express.static('public'));
 
-
-//Access Origin
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-
 //Socket setup
 var io = socket(server);
 
