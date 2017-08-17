76b5fc78ed83fd5f2adb490fc23c494e081791a0
Serkan Aysan <serkanaysann@gmail.com>
2017-08-17 16:55:17 +0300

    disabled secure

--- public/js/first.js
+++ public/js/first.js
@@ -19,7 +19,7 @@ function onOff(){
 
 var socketMove = function(options){
 	//Make connection
-	var socket = io.connect('http://arcelik.onedio.com/', {secure: true});
+	var socket = io.connect('http://arcelik.onedio.com/');
 
 	//Query DOM
 	var code = document.getElementById('code');
