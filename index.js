e87e9d091f710efb692ac167d8e024239264b2b6
Serkan Aysan <serkanaysann@gmail.com>
2017-08-17 15:59:24 +0300

    change socket 443

--- index.js
+++ index.js
@@ -4,32 +4,12 @@ var socket = require('socket.io');
 
 //App setup
 var app = express();
-var server = app.listen(8080, function(){
-	console.log('listening to request on port 8080');
+var server = app.listen(443, function(){
+	console.log('listening to request on port 443');
 });
 
 //Static files
 app.use(express.static('public'));
-
-// Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 
 //Socket setup
 var io = socket(server);
